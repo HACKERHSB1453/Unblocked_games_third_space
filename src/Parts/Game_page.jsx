0ec41b3c2f717d4e0543dec4d7b5d54b68b9c7e8
unknown <rogerpro52010@gmail.com>
2026-09-15 22:05:52 +0200

    GamePage Edit

--- src/Parts/Game_page.jsx
+++ src/Parts/Game_page.jsx
@@ -1,19 +1,27 @@
 function Game_page () {
-    return(
-    <div>
-        <h2>
-            Example game title
-        </h2>
-        <div>
-            <p>Here goes the game itself</p>
-        </div>
-        <h3>
-            Here goes the game desctiption
-        </h3>
-        <div>
-            <p>Inside here should be recomended or more games</p>
-        </div>
-    </div>
+    return (
+        <>
+            <div>
+                <h2>
+                    Example game title
+                </h2>
+                <div>
+                    <p>Here goes the game itself</p>
+                </div>
+                <h3>
+                    Here goes the game desctiption
+                </h3>
+                <div>
+                    <p>Inside here should be recomended or more games</p>
+                </div>
+            </div>
+
+            <section>
+                <h1>
+                    Game's Name for SEO
+                </h1>
+            </section>
+        </>
     );
 }
 export default Game_page;
