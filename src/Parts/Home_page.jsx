934869cce154e17200e941b34d117a20afb69dbd
HakanHSB <hakansbaser@gmail.com>
2026-09-15 15:30:19 +0200

    I added a home page

---
+++ src/Parts/Home_page.jsx
@@ -0,0 +1,10 @@
+function Hompage() {
+  return (
+    <div>
+      <h1 id="home_page_title">Welcome to the Homepage</h1>
+      <h3>Here you can play games and also upload your own!</h3>
+    </div>
+  );
+}
+
+export default Hompage;
